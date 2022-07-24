9373d08f83160bcd28fa04ba71651d39c123505a
Grapefruit12 <midnight3655@gmail.com>
2022-07-24 19:08:28 +0900

    add card and edit from component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,8 +6,41 @@ import styles from './maker.module.css';
 import {useNavigate} from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
+import { useState } from 'react';
 
 const Maker = ({authService}) => {
+    const [cards, setCards] =useState([
+        {
+            id:'1',
+            name: 'Ellie',
+            company: 'Samsung',
+            theme: 'dark',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL: null,
+        },
+        {
+            id:'2',
+            name: 'Ellie2',
+            company: 'Samsung',
+            theme: 'light',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL: null,
+        },
+        {
+            id:'3',
+            name: 'Ellie3',
+            company: 'Samsung',
+            theme: 'colorful',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL: null,
+        },
+    ]);
     const history=useNavigate();
     const onLogout=()=>{
         authService.logout();
@@ -24,8 +57,8 @@ const Maker = ({authService}) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
                 <div className={styles.container}>
-                    <Editor />
-                    <Preview />
+                    <Editor cards={cards}/>
+                    <Preview cards={cards}/>
                 </div>
             <Footer />
         </section>
